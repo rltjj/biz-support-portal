81f452c66474ab7a2a85b32c8566062050a4cc70
rltjj <rlt231022@gmail.com>
2025-09-10 10:02:35 +0900

    페이지 이동 구현

---
+++ js/script.js
@@ -0,0 +1,14 @@
+async function loadHTML(id, file) {
+  try {
+    const res = await fetch(file);
+    if (!res.ok) throw new Error(`Failed to fetch ${file}`);
+    document.getElementById(id).innerHTML = await res.text();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadHTML("header", "header.html");
+  loadHTML("footer", "footer.html");
+});
